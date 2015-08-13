f9d4757a3f4fd820b6d6bed3ab3df5c1199ce0f1
Marco Solazzi <msolazzi@aquest.it>
2015-08-13 14:03:50 +0200

    base stuff

--- frontend/assets/js/application.js
+++ frontend/assets/js/application.js
@@ -5,20 +5,3 @@
  * or just fill it with JS on small projects
  *
  */
-
-var angular = require('angular');
-
-require('./base/plugins');
-
-let todoListApp;
-
-todoListApp = angular.module('todoListApp', [require('angular-resource'), require('angular-animate')])
-    .factory('todoService', require('./services/todo'))
-    .controller('ListController', require('./layout/list.controller'))
-    .controller('MainController', require('./layout/main.controller'))
-    .directive('myProgress', require('./components/progress/progress.directive'))
-    .directive('myTodo', require('./components/todo/todo.directive'))
-    .directive('myForm', require('./components/form/form.directive'))
-    .animation('.todo-item', require('./components/todo/todo.animation'));
-
-module.exports = todoListApp;
